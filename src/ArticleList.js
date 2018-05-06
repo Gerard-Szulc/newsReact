745faab7adc2cb05ad8da5baeb18fdf48e111f8e
Gerard Szulc <gerszulc05@gmail.com>
2018-05-06 13:34:55 +0200

    article start

--- src/ArticleList.js
+++ src/ArticleList.js
@@ -6,6 +6,7 @@ export default class ArticleList extends Component {
   render(){
     return(
       <Article
+        article={item}
         data={this.props.data}
         keyExtractor={item=>item.url}
         refreshing={this.props.refreshing}
--- src/components/articles.js
+++ src/components/articles.js
@@ -4,6 +4,40 @@ import React,{Component} from 'react'
 
 export default class Article extends Component{
   render(){
-    return()
+    const {
+      title,
+      description,
+      publishedAt,
+      source,
+      urlToImage,
+      url
+    } = this.props.article;
+
+    const { noteStyle, featuredTitleStyle } = styles;
+    const time = moment(publishedAt || moment.now()).fromNow();
+    const defaultImg =
+      'https://wallpaper.wiki/wp-content/uploads/2017/04/wallpaper.wiki-Images-HD-Diamond-Pattern-PIC-WPB009691.jpg';
+
+
+
+    return(
+      <div onClick={() => Linking.openURL(url)}>
+        <div
+        featuredTitle={title}
+        featuredTitleStyle={featuredTitleStyle}
+        image={{
+          uri: urlToImage || defaultImg
+        }}
+        >
+          <p style={{marginBottom: 10}}>
+            {description||'REad More...'}
+          </p>
+
+
+
+        </div>
+      </div>
+
+    )
   }
 }
