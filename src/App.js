d50acdcc717cfd7a14f231fc7fcaec1933ce009b
Gerard Szulc <gerszulc05@gmail.com>
2018-05-06 20:52:45 +0200

    add date pf article

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
 import React, {Component} from 'react';
 import {getNews} from "./news";
-
+import Moment from 'react-moment'
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {articles: [], refreshing: true}
-    this.fetchNews = this.fetchNews.bind(this)
 
-  }
+ state = {articles: [], refreshing: true};
+
+
 
   componentDidMount() {
     this.fetchNews();
   }
 
-  fetchNews() {
-    getNews()
-      .then(articles => this.setState({articles, refreshing: false}
-      ))
-      .catch(() => this.setState({refreshing: false}))
+  fetchNews(){
+    getNews().then(articles => this.setState({articles, refreshing: false}
+      )).catch(() => this.setState({refreshing: false}));
+
   }
 
   handleRefresh = () =>{
-    this.setState({
-        refreshing: true
-      },
-      ()=>{this.fetchNews()}
-    )
-  }
+    this.setState({refreshing: true},
+      ()=>this.fetchNews()
+      )
+
+  };
+
 
 
 
   render() {
       return (
-        <div >
+        <div>
         <button onClick={this.handleRefresh}>Reload</button>
         <div style={{display: 'flex', justifyContent: 'space-between',
         flexWrap: 'wrap'}}>
           {
             this.state.articles.map(
-              (article,index) => (
+              (article,index) =>(
 
-
-              <div key={index}>
-                <a href={article.url} style={{}}>
-                <img style={{width: 320, height: 180}} src={article.urlToImage}/>
-                <h1 style={{fontSize: 15, maxWidth: 160}}> {article.title}</h1>
+              <div key={index} style={{margin: 10}}>
+                <a href={article.url} style={{color: 'black', textDecoration: 'none'}}>
+                <img style={{width: 320, height: 180}} src={article.urlToImage} alt={article.title}/>
+                <h1 style={{fontSize: 15, maxWidth: 320}}> {article.title}</h1>
                 </a>
+                <p style={{fontSize: 12, maxWidth: 320}}>{article.description}</p>
                 <p style={{fontSize: 10}}>{article.author}</p>
-
-                <p style={{fontSize: 10, maxWidth: 160}}>{article.description}</p>
+                <Moment format="YYYY/MM/DD" date={article.publishedAt} style={{fontSize: 10}}/>
                 </div>
 
               )
