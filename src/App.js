b6c54503dc3e0482a34f3c169c71ae24485282ab
Gerard Szulc <gerszulc05@gmail.com>
2018-05-06 19:28:48 +0200

    changed structure.. still learning

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 import {getNews} from "./news";
 
-import logo from './logo.svg';
 import './App.css';
-import ArticleList from "./ArticleList";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {articles: [], refreshing: true}
     this.fetchNews = this.fetchNews.bind(this)
+
   }
 
   componentDidMount() {
@@ -18,28 +17,52 @@ class App extends Component {
 
   fetchNews() {
     getNews()
-      .then(articles => this.setState({articles, refreshing: false}))
+      .then(articles => this.setState({articles, refreshing: false}
+      ))
       .catch(() => this.setState({refreshing: false}))
   }
 
-  handleRefresh() {
+  handleRefresh = () =>{
     this.setState({
         refreshing: true
-      }, () => this.fetchNews()
+      },
+      ()=>{this.fetchNews()}
     )
   }
 
+
+
   render() {
-    return (
-      <div>
-        <ArticleList
-          data={this.state.articles}
-          refreshing={this.props.refreshing}
-          onRefresh={this.props.handleRefresh.bind(this)}
-        />
-      </div>
-    );
+      return (
+        <div>
+        <button onClick={this.handleRefresh}>Reload</button>
+        <ul>
+          {
+            this.state.articles.map(
+              (article,index) => (
+              <li key={index}>
+                <img style={{width: 160, height: 90}} src={article.urlToImage}/>
+                <h1> {article.title}</h1>
+                <p>{article.author}</p>
+
+                </li>
+              )
+
+
+            )
+          }
+
+        </ul>
+        </div>
+
+
+        )
   }
+
+
+
+
+
 }
 
 export default App;
