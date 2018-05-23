3679714a4516a7451fcf4497ac7ba63f7be37c42
Gerard Szulc <gerszulc05@gmail.com>
2018-05-23 22:40:35 +0200

    colors on article containers

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,25 +6,27 @@ export default class Article extends Component {
     return (
       <div>
         <button onClick={this.props.handleRefresh}>Reload</button>
-        <div className="news-container">
+        <div className={'news-container'} style={{marginLeft: 'auto', marginRight: 'auto'}}>
           {
             this.props.articles.map(
-              (article, index) => (
-
-                <div key={index} style={{margin: 20}}>
+              (article, index) => {
+                  const backg = '#' + parseInt(Math.min(index*8+100,255),16)
+               return (
+               <div key={index} style={{padding: 10,margin: 0, backgroundColor: backg,}}>
                   <a href={article.url} style={{color: 'black', textDecoration: 'none'}}>
                     {article.urlToImage !== null ?
-                      <img style={{maxWidth: 320, maxHeight: 180}} src={ article.urlToImage} alt={article.title.slice(0,15)}/>:
-                      <img style={{maxWidth: 320, maxHeight: 180}} src={'https://cst.org.uk/data/image/8/e/8e3e848cbd24bdb85a7c97869ec77386.1451995352.jpg'} alt={article.title.slice(0,15)}/>
+                      <img style={{display: 'block',maxWidth: 320, maxHeight: 180, marginLeft: 'auto', marginRight: 'auto'}} src={ article.urlToImage} alt={article.title.slice(0,15)}/>:
+                      <img style={{display: 'block' ,maxWidth: 320, maxHeight: 180, marginLeft: 'auto', marginRight: 'auto'}} src={'https://cst.org.uk/data/image/8/e/8e3e848cbd24bdb85a7c97869ec77386.1451995352.jpg'} alt={article.title.slice(0,15)}/>
                     }
                     <h1 style={{fontSize: 15, maxWidth: 320}}> {article.title}</h1>
                   </a>
+                  <p style={{fontSize: 9, maxWidth: 320}}>{article.source.name}</p>
                   <p style={{fontSize: 12, minWidth: 250, maxWidth: 320}}>{article.description}</p>
                   <p style={{fontSize: 10, maxWidth: 320}}>{article.author}</p>
                   <Moment format="YYYY/MM/DD" date={article.publishedAt} style={{fontSize: 10}}/>
-                </div>
+                </div>)
 
-              )
+                  }
             )
           }
 
