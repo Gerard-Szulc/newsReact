528f2f610960099aa5bce446728fb700e140990c
Gerard-Szulc <gerszulc05@gmail.com>
2019-05-27 21:56:11 +0200

    ignore files and remove cached files with git rm --cached FILENAME

--- src/config/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyC9Kqj9nYbZIyLpQOkck80R6ilV5VDOZEY",
-    authDomain: "stronka-z-newsami-i-pogoda.firebaseapp.com",
-    databaseURL: "https://stronka-z-newsami-i-pogoda.firebaseio.com",
-    projectId: "stronka-z-newsami-i-pogoda",
-    storageBucket: "",
-    messagingSenderId: "504518117423"
-  };
